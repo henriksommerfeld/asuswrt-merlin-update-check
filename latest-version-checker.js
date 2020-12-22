2fd8f5ec1dc8bbb847295119c292d2ed9877ee46
Henrik Sommerfeld <henrik@sommerfeld.nu>
2020-12-22 21:34:47 +0100

    Adjusted to website change

--- latest-version-checker.js
+++ latest-version-checker.js
@@ -4,13 +4,13 @@ import $ from 'cheerio';
 export async function getLatestStableVersion() {
   try {
     const html = await rp('https://www.asuswrt-merlin.net/');
-    const text = $('#block-currentrelease', html).text();
-    const rows = text.split('\n');
-    const stableOthers = rows.find(r => r.startsWith('Others:'));
-    const stableVersion = stableOthers.split('Others:')[1].trim();
+    const stableVersion = $('table tbody tr', html)
+      .first()
+      .text()
+      .split('\n')[2]
+      .trim();
     return stableVersion;
-  }
-  catch (error) {
+  } catch (error) {
     Promise.reject(error);
   }
 }
--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 import { getLatestStableVersion } from './latest-version-checker.js';
 import { sendPushoverNotification } from './notify.js';
 import { getLastCheckedVersion, saveLastCheckedVersion } from './localFile.js';
@@ -6,27 +5,24 @@ import { getLastCheckedVersion, saveLastCheckedVersion } from './localFile.js';
 async function main() {
   try {
     const lastCheckedVersion = getLastCheckedVersion();
-    console.log("main -> lastCheckedVersion", lastCheckedVersion)
+    console.log('main -> lastCheckedVersion', lastCheckedVersion);
     const latestVersion = await getLatestStableVersion();
-    
+
     if (latestVersion !== lastCheckedVersion) {
       const message = `🔔 New firmware version ${latestVersion} is now available at 
       
       https://www.asuswrt-merlin.net/`;
       sendPushoverNotification(message);
       saveLastCheckedVersion(latestVersion);
-    }
-    else {
+    } else {
       const message = `🤷‍♂️ No firmware released. ${latestVersion} is the latest.`;
       sendPushoverNotification(message);
     }
   } catch (error) {
-    console.log("main -> error", error)
+    console.log('main -> error', error);
     const message = `⚠️ Router firmware update check failed`;
     sendPushoverNotification(message);
   }
 }
 
 main();
-  
-  
